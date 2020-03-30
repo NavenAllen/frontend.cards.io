cfca080d75f2e5b9b3ddd47eb8508aa128d00fb5
Naven Rajkumar <naven1999@gmail.com>
2020-03-30 15:50:14 +0530

    Feat: Add actions and reducers for literature game

--- src/literature/state/actions/game.actions.js
+++ src/literature/state/actions/game.actions.js
@@ -3,18 +3,32 @@ import socket from '../../../util/socket-client'
 export const literatureGameConstants = {
 	PLAY_ASK_REQUEST: 'PLAY_ASK_REQUEST',
 	PLAY_ASK_SUCCESS: 'PLAY_ASK_SUCCESS',
-	PLAY_ASK_FAILURE: 'PLAY_ASK_FAILURE'
+	PLAY_ASK_FAILURE: 'PLAY_ASK_FAILURE',
+
+	PLAY_DECLARE_REQUEST: 'PLAY_DECLARE_REQUEST',
+	PLAY_DECLARE_SUCCESS: 'PLAY_DECLARE_SUCCESS',
+	PLAY_DECLARE_FAILURE: 'PLAY_DECLARE_FAILURE',
+
+	PLAY_TRANSFER_REQUEST: 'PLAY_TRANSFER_REQUEST',
+	PLAY_TRANSFER_SUCCESS: 'PLAY_TRANSFER_SUCCESS',
+	PLAY_TRANSFER_FAILURE: 'PLAY_TRANSFER_FAILURE'
 }
 
 export const literatureGameActions = {
 	playAsk,
 	playAskSuccess,
-	playAskFailure
+	playAskFailure,
+	playDeclare,
+	playDeclareSuccess,
+	playDeclareFailure,
+	playTransfer,
+	playTransferSuccess,
+	playTransferFailure
 }
 
 function playAsk(askDetails) {
 	return (dispatch) => {
-		dispatch({ type: literatureGameConstants.CREATE_GAME_REQUEST })
+		dispatch({ type: literatureGameConstants.PLAY_ASK_REQUEST })
 
 		socket.emit('play-ask', askDetails)
 	}
@@ -22,13 +36,55 @@ function playAsk(askDetails) {
 
 function playAskSuccess() {
 	return (dispatch) =>
-		dispatch({ type: literatureGameConstants.CREATE_GAME_SUCCESS })
+		dispatch({ type: literatureGameConstants.PLAY_ASK_SUCCESS })
 }
 
 function playAskFailure(errorDetails) {
 	return (dispatch) =>
 		dispatch({
-			type: literatureGameConstants.CREATE_GAME_FAILURE,
+			type: literatureGameConstants.PLAY_ASK_FAILURE,
+			errorDetails
+		})
+}
+
+function playDeclare(declareDetails) {
+	return (dispatch) => {
+		dispatch({ type: literatureGameConstants.PLAY_DECLARE_REQUEST })
+
+		socket.emit('play-declare', declareDetails)
+	}
+}
+
+function playDeclareSuccess() {
+	return (dispatch) =>
+		dispatch({ type: literatureGameConstants.PLAY_DECLARE_SUCCESS })
+}
+
+function playDeclareFailure(errorDetails) {
+	return (dispatch) =>
+		dispatch({
+			type: literatureGameConstants.PLAY_DECLARE_FAILURE,
+			errorDetails
+		})
+}
+
+function playTransfer(transferDetails) {
+	return (dispatch) => {
+		dispatch({ type: literatureGameConstants.PLAY_TRANSFER_REQUEST })
+
+		socket.emit('play-transfer', transferDetails)
+	}
+}
+
+function playTransferSuccess() {
+	return (dispatch) =>
+		dispatch({ type: literatureGameConstants.PLAY_TRANSFER_SUCCESS })
+}
+
+function playTransferFailure(errorDetails) {
+	return (dispatch) =>
+		dispatch({
+			type: literatureGameConstants.PLAY_TRANSFER_FAILURE,
 			errorDetails
 		})
 }
--- src/literature/state/reducers/game.reducer.js
+++ src/literature/state/reducers/game.reducer.js
@@ -7,6 +7,7 @@ export function game(state = initialState, action) {
 		case literatureGameConstants.PLAY_ASK_REQUEST:
 			return {
 				...state,
+				error: null,
 				locked: true
 			}
 		case literatureGameConstants.PLAY_ASK_SUCCESS:
@@ -16,6 +17,48 @@ export function game(state = initialState, action) {
 				locked: false
 			}
 		case literatureGameConstants.PLAY_ASK_FAILURE:
+			return {
+				...state,
+				error: {
+					code: action.data.code,
+					message: action.data.message
+				},
+				locked: false
+			}
+		case literatureGameConstants.PLAY_TRANSFER_REQUEST:
+			return {
+				...state,
+				error: null,
+				locked: true
+			}
+		case literatureGameConstants.PLAY_TRANSFER_SUCCESS:
+			return {
+				...state,
+				error: null,
+				locked: false
+			}
+		case literatureGameConstants.PLAY_TRANSFER_FAILURE:
+			return {
+				...state,
+				error: {
+					code: action.data.code,
+					message: action.data.message
+				},
+				locked: false
+			}
+		case literatureGameConstants.PLAY_DECLARE_REQUEST:
+			return {
+				...state,
+				error: null,
+				locked: true
+			}
+		case literatureGameConstants.PLAY_DECLARE_SUCCESS:
+			return {
+				...state,
+				error: null,
+				locked: false
+			}
+		case literatureGameConstants.PLAY_DECLARE_FAILURE:
 			return {
 				...state,
 				error: {
