6820a33b6d13687ce7a1c1d4b02275e40bbe2f4e
Nandha Kishore <jnandhakishore12@gmail.com>
2020-04-01 21:20:49 +0530

    Feat: Beautify HomePage

--- src/game-engine/components/HomePage/HomePage.js
+++ src/game-engine/components/HomePage/HomePage.js
@@ -9,10 +9,10 @@ import { gameActions } from '../../state/actions'
 import { withStyles } from '@material-ui/core/styles'
 import {
 	Container,
+	Divider,
 	Grid,
 	Paper,
 	TextField,
-	Typography
 } from '@material-ui/core'
 
 const styles = (theme) => ({
@@ -34,8 +34,6 @@ const styles = (theme) => ({
 	paper: {
 		boxShadow: theme.shadows[9]
 	},
-	mainContainer: {
-	},
 	mainContainer: {},
 	mainGrid: {
 		marginTop: theme.spacing(2)
@@ -43,22 +41,40 @@ const styles = (theme) => ({
 	mainPaper: {
 		marginTop: theme.spacing(19),
 		//minHeight: "70vh",
-		backgroundColor: "#fff",
-		//backgroundColor: "rgba(255,255,255,0.4)",
-		//filter: "blur(4px)",
+		// backgroundColor: "#fff",
+		backgroundColor: 'rgba(255,255,255,0.65)',
+		backdropFilter: 'blur(4px)'
 	},
 	paperGridContainer: {
 		padding: theme.spacing(2)
 	},
 	nameBigContainer: {
 		marginTop: theme.spacing(0.5),
-		marginBottom: theme.spacing(4)
+		marginBottom: theme.spacing(2)
 	},
 	sectionGrid: {
-		padding: theme.spacing(2),
+		padding: theme.spacing(1),
+		alignContent: 'center'
 	},
 	nameInputContainer: {
-		marginTop: theme.spacing(2)
+		marginTop: theme.spacing(0.5)
+	},
+	cssLabel: {
+		color: 'black',
+		fontSize: '0.9rem'
+	},
+	cssOutlinedInput: {
+		'&$cssFocused $notchedOutline': {
+			borderWidth: '2px',
+			borderColor: `${theme.palette.primary.main} !important`
+		}
+	},
+	cssFocused: {
+		fontSize: '1rem'
+	},
+	notchedOutline: {
+		borderWidth: '2px',
+		borderColor: 'black !important'
 	}
 })
 
@@ -83,56 +99,67 @@ const HomePage = (props) => {
 	const handleNameInputChange = (e) => {
 		setName(e.target.value)
 	}
+
 	const { classes, locked } = props
 	return (
-		<Container component="main" className={classes.mainContainer}>
-			<Grid
-				container
-				spacing={0}
-				direction="column"
-				alignItems="center"
-				justify="center"
-				className={classes.mainGrid}
-			>
-				<Paper className={classes.mainPaper} elevation={6}>
-					<Grid container className={classes.paperGridContainer}>
-						<Grid xs={12} className={classes.titleContainer}>
-							<Typography
-								variant="h3"
-								component="h3"
-								className={classes.title}
-							>
-								cards.io
-							</Typography>
-						</Grid>
-						<Grid
-							container
-							xs={12}
-							sm={12}
-							xl={12}
-							justify="center"
-							className={classes.nameBigContainer}
-						>
-							<Grid item xs={2} sm={4} xl={4}></Grid>
+			<Container component="main" className={classes.mainContainer}>
+				<Grid
+					container
+					spacing={0}
+					direction="column"
+					alignItems="center"
+					justify="center"
+					className={classes.mainGrid}
+				>
+					<Paper className={classes.mainPaper} elevation={6}>
+						<Grid container className={classes.paperGridContainer}>
+							{/* <Grid xs={12} className={classes.titleContainer}>
+								<Typography
+									variant="h3"
+									component="h3"
+									className={classes.title}
+								>
+									cards.io
+								</Typography>
+							</Grid> */}
 							<Grid
-								item
-								xs={8}
-								sm={4}
-								xl={4}
+								container
+								xs={12}
+								sm={12}
+								xl={12}
 								justify="center"
 								className={classes.joinBtnContainer}
 							>
 								<Grid item xs={2} sm={4} xl={4}></Grid>
-								<Grid item xs={8} sm={4} xl={4}
+								<Grid
+									item
+									container
+									xs={8}
+									sm={4}
+									xl={4}
 									justify="center"
 									className={classes.nameInputContainer}
 								>
 									<TextField
 										label="Enter your name"
 										size="small"
-										fullWidth
 										variant="outlined"
 										className={classes.textField}
+										InputLabelProps={{
+											classes: {
+												root: classes.cssLabel,
+												focused: classes.cssFocused
+											}
+										}}
+										InputProps={{
+											classes: {
+												root: classes.cssOutlinedInput,
+												focused: classes.cssFocused,
+												notchedOutline:
+													classes.notchedOutline
+											},
+											inputMode: 'text'
+										}}
 										color="primary"
 										value={name}
 										onChange={handleNameInputChange}
@@ -140,18 +167,39 @@ const HomePage = (props) => {
 								</Grid>
 								<Grid item xs={2} sm={4} xl={4}></Grid>
 							</Grid>
-							<Grid container xs={12} sm={6} className={classes.sectionGrid} spacing={1}>
-								<CreateGameForm
-									createGame={this.handleCreateGameFormSubmit}
-									locked={locked}
-								/>
-							</Grid>
-							<Grid container xs={12} sm={6} className={classes.sectionGrid}>
-								<JoinGameForm
-									joinGame={this.handleJoinGameFormSubmit}
-									probeGameRequest={this.props.probeGameRequest}
-									players={this.props.players}
-								/>
+							<Grid container xs={12}>
+								<Grid
+									container
+									xs
+									className={classes.sectionGrid}
+								>
+									<CreateGameForm
+										createGame={
+											this.handleCreateGameFormSubmit
+										}
+										locked={locked}
+									/>
+								</Grid>
+								<Grid container justify="center" item xs={1}>
+									<Divider
+										orientation="vertical"
+										variant="middle"
+										flexItem
+									/>
+								</Grid>
+								<Grid
+									container
+									xs
+									className={classes.sectionGrid}
+								>
+									<JoinGameForm
+										joinGame={this.handleJoinGameFormSubmit}
+										probeGameRequest={
+											this.props.probeGameRequest
+										}
+										players={this.props.players}
+									/>
+								</Grid>
 							</Grid>
 							<Grid item xs={2} sm={4} xl={4}></Grid>
 						</Grid>
@@ -173,10 +221,9 @@ const HomePage = (props) => {
 								players={props.players}
 							/>
 						</Grid>
-					</Grid>
-				</Paper>
-			</Grid>
-		</Container>
+					</Paper>
+				</Grid>
+			</Container>
 	)
 }
 
