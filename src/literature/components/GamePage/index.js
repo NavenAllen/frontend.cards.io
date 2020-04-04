c009a815af368f5b973be7f8502f6ec11f31be6a
Tejus <rameshtejus@gmail.com>
2020-04-04 17:41:50 +0530

    [feat] Add loader

--- src/literature/components/GamePage/index.js
+++ src/literature/components/GamePage/index.js
@@ -1,9 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
 
 import './GamePage.css'
 import { Engine } from '../../../game-engine/engine'
 import { Declare } from '../Declare'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import WaitingRoom from '../../../game-engine/components/WaitingRoom/WaitingRoom'
+import AskCard from '../AskCard/AskCard'
+import LoaderModal from '../../../game-engine/components/LoaderModal/LoaderModal'
 
 const GamePage = (props) => {
 	const player = useSelector((state) => state.playerData)
@@ -27,15 +32,57 @@ const GamePage = (props) => {
 		userPos: player.position
 	})
 	const [open, setOpen] = useState(false)
+	const [gameLoaded, setGameLoaded] = useState(false)
 	const handleClose = () => {
 		setOpen((prev) => !prev)
 	}
+	useEffect(() => {
+		if (props.inGame) setGameLoaded(true)
+	})
 	return (
 		<>
-			{game}
-			{open && <Declare open={open} handleClose={handleClose} />}
+			{gameLoaded ? (
+				<div>
+					{!props.gameData.isActive ? (
+						<WaitingRoom
+							players={props.gameData.players}
+							gameCode={props.gameData.code}
+							playerData={props.playerData}
+						/>
+					) : (
+						<>
+							{game}
+							{open && (
+								<Declare
+									open={open}
+									handleClose={handleClose}
+								/>
+							)}
+							{props.cardSelected != undefined ? (
+								<AskCard />
+							) : null}
+						</>
+					)}
+				</div>
+			) : (
+				<LoaderModal show={true} />
+			)}
 		</>
 	)
 }
+GamePage.propTypes = {
+	gameData: PropTypes.object.isRequired,
+	cardSelected: PropTypes.object,
+	playerData: PropTypes.object,
+	inGame: PropTypes.bool.isRequired
+}
+const mapStateToProps = (state) => {
+	return {
+		gameData: state.gameData,
+		cardSelected: state.cardSelected,
+		playerData: state.playerData,
+		inGame: state.inGame
+	}
+}
 
-export default GamePage
+export default connect(mapStateToProps, null)(GamePage)
