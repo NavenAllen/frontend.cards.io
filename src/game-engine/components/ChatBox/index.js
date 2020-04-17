71713227028ad5dc2153650e04da673ed92a82e4
shohan <shohanduttaroy99@gmail.com>
2020-04-17 18:28:52 +0530

    [feat] Add unread message badge and send on enter

--- src/game-engine/components/ChatBox/index.js
+++ src/game-engine/components/ChatBox/index.js
@@ -11,22 +11,29 @@ import {
 } from '@material-ui/core'
 import ChatIcon from '@material-ui/icons/Chat'
 import SendIcon from '@material-ui/icons/Send'
+import MinimizeIcon from '@material-ui/icons/Minimize'
 import { useSelector, useDispatch } from 'react-redux'
-import styles from './style.module.css'
 import { gameActions } from '../../state/actions/game.actions'
-import classes from './style.module.css'
+import styles from './style.module.css'
 
-const Chat = () => {
+export const ChatBox = () => {
+	const [openChat, setOpenChat] = useState(true)
 	const box = useRef()
 	const message = useRef()
 	const dispatch = useDispatch()
 	const user = useSelector((state) => state.playerData)
 	const chat = useSelector((state) => state.chats)
 	const game = useSelector((state) => state.gameData)
+	const [chatLen, setChatLen] = useState(0)
 	useEffect(() => {
 		box.current.scrollIntoView()
+		if (openChat) setChatLen(chat.length)
 	}, [chat])
-	const send = () => {
+	useEffect(() => {
+		if (openChat) setChatLen(chat.length)
+	}, [openChat])
+	const send = (e) => {
+		e.preventDefault()
 		if (message.current.value === '') return
 		dispatch(
 			gameActions.sendChat({
@@ -38,58 +45,6 @@ const Chat = () => {
 		message.current.value = ''
 	}
 	return (
-		<Paper className={styles.chatBox}>
-			<header className={styles.chatHeader}>
-				{/* {chat.length === 0
-					? 'Send a message and get chatting!'
-					: 'Chat'} */}
-				Chat
-			</header>
-			<List className={styles.chat}>
-				{chat.map((message, index) => (
-					<ListItem className={styles.message}>
-						<ListItemText
-							classes={{ primary: styles.chatName }}
-							className={`${styles.messageText} ${
-								message.player.name === user.name &&
-								styles.ownerMessageText
-							}`}
-							primary={message.player.name}
-							secondary={message.message}
-						/>
-					</ListItem>
-				))}
-				<small
-					className={chat.length === 0 ? styles.noMessage : null}
-					ref={box}
-				>
-					{chat.length === 0 && 'Send a message and get chatting!'}
-				</small>
-			</List>
-			<div className={styles.textBoxContainer}>
-				<InputBase
-					className={styles.textBox}
-					placeholder="Send message..."
-					inputProps={{ 'aria-label': 'Send message...' }}
-					inputRef={message}
-					onSubmit={send}
-				/>
-				<IconButton
-					type="submit"
-					className={styles.iconButton}
-					aria-label="search"
-					onClick={send}
-				>
-					<SendIcon />
-				</IconButton>
-			</div>
-		</Paper>
-	)
-}
-
-export const ChatBox = () => {
-	const [openChat, setOpenChat] = useState(true)
-	return (
 		<div className={styles.actionsFab}>
 			<>
 				<Collapse
@@ -97,7 +52,63 @@ export const ChatBox = () => {
 					in={openChat}
 					className={styles.collapseContainer}
 				>
-					<Chat />
+					<Paper className={styles.chatBox}>
+						<header className={styles.chatHeader}>
+							<span>Chat</span>
+							<IconButton
+								type="submit"
+								className={styles.minimize}
+								aria-label="search"
+								onClick={() => setOpenChat(false)}
+							>
+								<MinimizeIcon />
+							</IconButton>
+						</header>
+						<List className={styles.chat}>
+							{chat.map((message, index) => (
+								<ListItem className={styles.message}>
+									<ListItemText
+										classes={{ primary: styles.chatName }}
+										className={`${styles.messageText} ${
+											message.player.name === user.name &&
+											styles.ownerMessageText
+										}`}
+										primary={message.player.name}
+										secondary={message.message}
+									/>
+								</ListItem>
+							))}
+							<small
+								className={
+									chat.length === 0 ? styles.noMessage : null
+								}
+								ref={box}
+							>
+								{chat.length === 0 &&
+									'Send a message and get chatting!'}
+							</small>
+						</List>
+						<div className={styles.textBoxContainer}>
+							<form onSubmit={send}>
+								<InputBase
+									className={styles.textBox}
+									placeholder="Send message..."
+									inputProps={{
+										'aria-label': 'Send message...'
+									}}
+									inputRef={message}
+								/>
+								<IconButton
+									type="submit"
+									className={styles.iconButton}
+									aria-label="search"
+									onClick={send}
+								>
+									<SendIcon />
+								</IconButton>
+							</form>
+						</div>
+					</Paper>
 				</Collapse>
 				<Fab
 					variant="extended"
@@ -105,6 +116,11 @@ export const ChatBox = () => {
 					aria-label="add"
 					onClick={() => setOpenChat((prev) => !prev)}
 				>
+					{chat.length - chatLen > 0 && (
+						<span className={styles.unread}>
+							{chat.length - chatLen}
+						</span>
+					)}
 					<ChatIcon />
 					{window.innerWidth > 600 && (
 						<span style={{ marginLeft: 8 }}>CHAT</span>
